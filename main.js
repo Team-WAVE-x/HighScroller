a4f86b5ba1d0261a2a09eefad7ca692487d0efab
pmh-only <pmhstudio.pmh@gmail.com>
2020-08-04 21:30:40 +0900

    엣취

---
+++ main.js
@@ -0,0 +1,26 @@
+let timer, sw, scr
+const toastElList = [].slice.call(document.querySelectorAll('.toast'))
+const toastList = toastElList.map((toastEl) => {
+  return new bootstrap.Toast(toastEl, { autohide: false })
+})
+console.log(toastList)
+
+
+document.addEventListener('wheel', () => {
+  scr++
+  if (!sw) {
+    timer = scr = 0
+    setInterval(() => {
+      document.getElementsByClassName('tg1')[0].innerHTML = (scr * 12) +  ' rpm'
+      document.getElementsByClassName('tag')[0].style.color = 'rgb(255,' + (255 - scr * 5) + ',' + (255 - scr * 5) + ')'
+      document.getElementsByTagName('body')[0].style.backgroundColor = 'rgb(52,58,' + (64 + scr * 5) + ')'
+      scr = scr = 0
+      timer += 1
+
+      if (timer % 10 <= 0) {
+        if (toastList[timer / 10 - 1]) toastList[timer / 10 - 1].show()
+      }
+    }, 500)
+    sw = true
+  }
+})
